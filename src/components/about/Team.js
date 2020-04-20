4a3be077da2e9aad787f471eaaefd72fe6ba5eec
Nikita Chernov <drimchansky@gmail.com>
2020-04-20 12:20:41 +0300

    Create team

--- src/components/about/Team.js
+++ src/components/about/Team.js
@@ -4,38 +4,79 @@ import styled from 'styled-components'
 import { Grid, Container } from '@material-ui/core'
 // components
 import TeamItem from './TeamItem'
+// custom
+import { colors } from '../../utility/variables'
 
 const Team = () => {
+  // temporary data
+  const data = [
+    { img: 'https://joeschmoe.io/api/v1/james', name: 'John', position: 'Designer' },
+    { img: 'https://joeschmoe.io/api/v1/jane', name: 'John', position: 'Designer' },
+    { img: 'https://joeschmoe.io/api/v1/julie', name: 'John', position: 'Designer' },
+    { img: 'https://joeschmoe.io/api/v1/jake', name: 'John', position: 'Designer' },
+    { img: 'https://joeschmoe.io/api/v1/josephine', name: 'John', position: 'Designer' },
+    { img: 'https://joeschmoe.io/api/v1/jean', name: 'John', position: 'Designer' },
+  ]
+
   return (
     <Section>
       <Container maxWidth="lg">
-        <Grid container>
-          <Grid item xs={12} sm={6} md={4}>
-            <TeamItem />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <TeamItem />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <TeamItem />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <TeamItem />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <TeamItem />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <TeamItem />
-          </Grid>
-        </Grid>
+        <Title>
+          <h2>Наша команда</h2>
+          <span>
+            Хотя Lorem ipsum напоминает классический латинский, вы не найдете никакого смысла в
+            сказанном.
+          </span>
+        </Title>
+        <GridStyled container spacing={5}>
+          {data.map(item => {
+            return (
+              <Grid item xs={12} sm={6} md={4}>
+                <TeamItem img={item.img} name={item.name} position={item.position} />
+              </Grid>
+            )
+          })}
+        </GridStyled>
       </Container>
     </Section>
   )
 }
 
 const Section = styled.section`
-  min-height: 100px;
+  @media (min-width: 960px) {
+    background: linear-gradient(0deg, ${colors.secondaryFour} 67%, #fff 0);
+  }
+`
+
+const Title = styled.div`
+  text-align: center;
+  padding: 2rem 0 1rem 0;
+  color: ${colors.secondaryTwo};
+  max-width: 600px;
+  margin: 0 auto;
+
+  @media (min-width: 600px) {
+    padding: 3rem 0 2rem 0;
+  }
+
+  & h2 {
+    font-size: 1.8rem;
+
+    @media (min-width: 960px) {
+      font-size: 2.5rem;
+    }
+  }
+
+  & span {
+    font-size: 1rem;
+    display: block;
+    margin-top: 0.5rem;
+    opacity: 0.9;
+  }
+`
+
+const GridStyled = styled(Grid)`
+  padding: 2rem 0 1rem 0;
 `
 
 export default Team
--- src/components/about/TeamItem.js
+++ src/components/about/TeamItem.js
@@ -1,16 +1,103 @@
+// libraries
 import React from 'react'
 import styled from 'styled-components'
+// components
+import { Codepen, Twitter, Dribble } from '../common/SVGIcons'
+// custom
+import { colors } from '../../utility/variables'
 
-const TeamItem = () => {
+const TeamItem = ({ img, name, position }) => {
   return (
     <ItemStyled>
-      <>Item</>
+      <ImgWrap>
+        <img src={img} alt="Avatar" />
+      </ImgWrap>
+      <Info>
+        <span>{name}</span>
+        <small>{position}</small>
+      </Info>
+      <Social>
+        <li>
+          <a href="/">
+            <Twitter fill="#00A8F0" size="24px" />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <Dribble fill="#F61067" size="24px" />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <Codepen size="24px" />
+          </a>
+        </li>
+      </Social>
     </ItemStyled>
   )
 }
 
+const ImgWrap = styled.div`
+  position: relative;
+  height: 0;
+  padding-bottom: 100%;
+
+  & img {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    left: 0;
+    top: 0;
+    border-radius: 50%;
+  }
+`
+
 const ItemStyled = styled.div`
-  background: gray;
+  max-width: 350px;
+  margin: 0 auto;
+`
+
+const Info = styled.div`
+  color: ${colors.secondaryTwo};
+  text-align: center;
+  margin-top: 0.5rem;
+
+  @media (min-width: 960px) {
+    color: ${colors.white};
+  }
+
+  & > span {
+    display: block;
+    font-weight: bold;
+    font-size: 1.3rem;
+  }
+
+  & > small {
+    display: block;
+    font-size: 1rem;
+    opacity: 0.9;
+    margin-top: 0.5rem;
+  }
+`
+
+const Social = styled.ul`
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+
+  & li:last-child {
+    svg {
+      fill: ${colors.secondaryFour};
+
+      @media (min-width: 960px) {
+        fill: ${colors.white};
+      }
+    }
+  }
+
+  & a {
+    padding: 1rem;
+  }
 `
 
 export default TeamItem
