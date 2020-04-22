9ffe23e80cf88d65e9f43993f99ffb75f18dbbcc
Nikita Chernov <drimchansky@gmail.com>
2020-04-23 00:27:13 +0300

    Add slick

--- src/components/about/TeamSlider.js
+++ src/components/about/TeamSlider.js
@@ -1,16 +1,63 @@
 // libraries
-import React from 'react'
+import React, { Component } from 'react'
+import Slider from 'react-slick'
 import styled from 'styled-components'
+import { Grid, Container } from '@material-ui/core'
 
-const TeamSlider = () => {
-  return (
-    <Section>
-      <>Team slider</>
-    </Section>
-  )
+import { colors } from '../../utility/variables'
+
+export default class TeamSlider extends Component {
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    }
+    return (
+      <Section>
+        <Container maxWidth="xl" disableGutters>
+          <Slider {...settings}>
+            <div>
+              <StyledSlide container>
+                <Grid item xs={12}>
+                  Content 1
+                </Grid>
+              </StyledSlide>
+            </div>
+            <div>
+              <StyledSlide container>
+                <Grid item xs={12}>
+                  Content 2
+                </Grid>
+              </StyledSlide>
+            </div>
+            <div>
+              <StyledSlide container>
+                <Grid item xs={12}>
+                  Content 3
+                </Grid>
+              </StyledSlide>
+            </div>
+            <div>
+              <StyledSlide container>
+                <Grid item xs={12}>
+                  Content 4
+                </Grid>
+              </StyledSlide>
+            </div>
+          </Slider>
+        </Container>
+      </Section>
+    )
+  }
 }
 
 const Section = styled.section`
-  min-height: 100px;
+  padding: 3rem 0;
 `
-export default TeamSlider
+
+const StyledSlide = styled(Grid)`
+  padding: 3rem 0;
+`
--- src/html.js
+++ src/html.js
@@ -13,6 +13,17 @@ export default function HTML(props) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
