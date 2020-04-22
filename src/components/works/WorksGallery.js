5da325b5fa03cc07438f012b799f2f7da15bc888
Nikita Chernov <drimchansky@gmail.com>
2020-04-22 22:22:38 +0300

    Works gallery in process

--- src/components/works/WorksGallery.js
+++ src/components/works/WorksGallery.js
@@ -22,18 +22,56 @@ const WorksGallery = () => {
       }
     }
   `)
-
-  console.log(tempData)
+  const tempImagesArray = tempData.allFile.nodes
 
   return (
     <Section>
-      <div>Work gallery</div>
+      <Container maxWidth="xl" disableGutters>
+        <Grid container component="ul">
+          {tempImagesArray.map(item => {
+            return (
+              <StyledItem key={Math.random()} item component="li" xs={12} sm={6}>
+                <WorksItem
+                  img={item.childImageSharp.fluid}
+                  title="Временный заголовок"
+                  description="Временный заголовок"
+                />
+              </StyledItem>
+            )
+          })}
+        </Grid>
+      </Container>
     </Section>
   )
 }
 
 const Section = styled.section`
-  min-height: 100px;
+  padding: 2rem 0;
+`
+
+const StyledItem = styled(Grid)`
+  position: relative;
+
+  &:before {
+    position: absolute;
+    content: '';
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #000;
+    mix-blend-mode: multiply;
+    opacity: 0.9;
+    z-index: 10;
+  }
+
+  @media (min-width: 600px) {
+    :nth-child(3n) {
+      width: 100%;
+      max-width: 100%;
+      flex-basis: 100%;
+    }
+  }
 `
 
 export default WorksGallery
--- src/components/works/WorksItem.js
+++ src/components/works/WorksItem.js
@@ -1,9 +1,64 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Grid } from '@material-ui/core'
+import Img from 'gatsby-image'
 
-const WorksItem = () => {
-  return <div></div>
+import { colors } from '../../utility/variables'
+
+const WorksItem = ({ img, title, description }) => {
+  console.log(img)
+  return (
+    <StyledItem>
+      <StyledImage fluid={img} alt="Work image" />
+      <a href="/">
+        <span>{title}</span>
+        <small>{description}</small>
+      </a>
+    </StyledItem>
+  )
 }
+
+const StyledItem = styled.div`
+  position: relative;
+  height: 30vw;
+  overflow: hidden;
+
+  & a {
+    position: absolute;
+    z-index: 100;
+    display: block;
+    height: 100%;
+    width: 100%;
+    text-decoration: none;
+    color: ${colors.white};
+
+    & span {
+      display: block;
+    }
+
+    & {
+      small {
+      }
+    }
+  }
+
+  &:hover img,
+  &:focus img {
+    transform: scale(1.1);
+  }
+`
+
+const StyledImage = styled(Img)`
+  /* Overwriting gatsby-image inline styles */
+  position: absolute !important;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+
+  & img {
+    /* Overwriting MaterialUI inline styles */
+    transition: transform 0.2s !important;
+  }
+`
 
 export default WorksItem
